bece593187a4665af67fbbdcad1a804626e2c312
si-tm <kumomaya@gmail.com>
2022-10-18 11:24:01 +0900

    入力ファイル移動

--- try_my_sample/run.sh
+++ try_my_sample/run.sh
@@ -5,12 +5,19 @@
 CODEDIR=..
 OXDNA_BIN=$CODEDIR/build/bin/oxDNA
 
+echo "make input files"
+TOPFILE=`find ../utils/testcase/ -name \*.top`
+CONFFILE=`find ../utils/testcase/ -name \*.conf`
+echo $TOPFILE
+echo $CONFFILE
+
+# make input file
+sed -i -e "s|TOP|$TOPFILE|" input
+sed -i -e "s|CONF|$CONFFILE|" input
+
+
 if [ -e $OXDNA_BIN ]
 then
-    # prepare the inputMD_seq_dep file
-    # sed "s;seq_dep_file = ../oxDNA1_sequence_dependent_parameters.txt;seq_dep_file = $CODEDIR\/oxDNA1_sequence_dependent_parameters.txt;" input_seq_dep > input.tmp
-    # mv -f input.tmp input_seq_dep
-
     # run the samples
     echo "Starting VMMC simulation with the sequence-averaged version of the model"
     $OXDNA_BIN input
@@ -23,4 +30,15 @@ then
 else
     echo "Can't find $OXDNA_BIN, did you compile oxDNA?"
     exit
+
 fi
+
+echo "move input and output files to results directory"
+
+cd ..
+mkdir results/
+cd try_my_sample/
+mv -f *.dat ../results/
+
+mv -f "$TOPFILE" ../results/
+mv -f "$CONFFILE" ../results/
