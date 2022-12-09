e35891d1e6731eb498a4b671d8159d83c30bbd85
Si-tm <57259352+si-tm@users.noreply.github.com>
2022-12-09 16:06:07 +0900

    Update tmp2.sh

--- tmp2.sh
+++ tmp2.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 RUN_FILE="/run_samples/make_samples_ked_1.sh"
+ls 
+ls run_samples/
 chmod 755 $RUN_FILE
 SEQ_FILE="/try_KakenhiEvolveDNA/seqA/seqA-GA100000-0.80_final_20200904131038.dat"
 SEQ_FILE="seqA-GA100000-0.80_final_20200904131038.dat"
