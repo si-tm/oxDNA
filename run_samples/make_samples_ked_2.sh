379a14a5fb05e54c3ca14d78edd56ec26ffd4678
Si-tm <57259352+si-tm@users.noreply.github.com>
2022-12-09 15:03:24 +0900

    Update make_samples_ked_2.sh

--- run_samples/make_samples_ked_2.sh
+++ run_samples/make_samples_ked_2.sh
@@ -43,46 +43,46 @@ mv generated.dat $CONFFILE
 SEQFILE=$1
 
 #### ここから ####
-sed -i -e "s|TOP|$TOPFILE|" $INPUTFILE
-sed -i -e "s|.dat|_${now}.dat|" $INPUTFILE
-sed -i -e "s|CONF|$CONFFILE|" $INPUTFILE
+# sed -i -e "s|TOP|$TOPFILE|" $INPUTFILE
+# sed -i -e "s|.dat|_${now}.dat|" $INPUTFILE
+# sed -i -e "s|CONF|$CONFFILE|" $INPUTFILE
 
 # input_seq_depファイル書き換え
 
-sed -i -e "s|TOP|$TOPFILE|" $SEQDEPFILE
-sed -i -e "s|.dat|_${now}.dat|" $SEQDEPFILE
-sed -i -e "s|CONF|$CONFFILE|" $SEQDEPFILE
+# sed -i -e "s|TOP|$TOPFILE|" $SEQDEPFILE
+# sed -i -e "s|.dat|_${now}.dat|" $SEQDEPFILE
+# sed -i -e "s|CONF|$CONFFILE|" $SEQDEPFILE
 
 # input_trapファイル書き換え
 
-sed -i -e "s|TOP|$TOPFILE|" $TRAPFILE
-sed -i -e "s|.dat|_${now}.dat|" $TRAPFILE
-sed -i -e "s|CONF|$CONFFILE|" $TRAPFILE
-sed -i -e "s|FORCE|${FORCESFILE}|" $TRAPFILE
-sed -i -e "/external_forces/c external_forces = 0|" $TRAPFILE # forceファイルを使わない設定
+# sed -i -e "s|TOP|$TOPFILE|" $TRAPFILE
+# sed -i -e "s|.dat|_${now}.dat|" $TRAPFILE
+# sed -i -e "s|CONF|$CONFFILE|" $TRAPFILE
+# sed -i -e "s|FORCE|${FORCESFILE}|" $TRAPFILE
+# sed -i -e "/external_forces/c external_forces = 0|" $TRAPFILE # forceファイルを使わない設定
 
 # # # 実行
-OXDNA_BIN=../build/bin/oxDNA
+# OXDNA_BIN=../build/bin/oxDNA
 
-if [ -e $OXDNA_BIN ]
-then
+# if [ -e $OXDNA_BIN ]
+# then
     # run the samples
-    echo "Starting VMMC simulation with the sequence-averaged version of the model"
-    echo $INPUTFILE
-    $OXDNA_BIN $INPUTFILE
+#     echo "Starting VMMC simulation with the sequence-averaged version of the model"
+#     echo $INPUTFILE
+#     $OXDNA_BIN $INPUTFILE
     
-    echo "Starting VMMC simulation with the sequence-dependent version of the model"
-    echo $SEQDEPFILE
-    $OXDNA_BIN $SEQDEPFILE
+#     echo "Starting VMMC simulation with the sequence-dependent version of the model"
+#     echo $SEQDEPFILE
+#     $OXDNA_BIN $SEQDEPFILE
 
-    echo "Starting VMMC simulation with the sequence-averaged version of the model and traps acting between nucleotides (see hairpin_forces.dat for details of the traps)"
-    echo $TRAPFILE
-    $OXDNA_BIN $TRAPFILE
-else
-    echo "Can't find $OXDNA_BIN, did you compile oxDNA?"
-    exit
+#     echo "Starting VMMC simulation with the sequence-averaged version of the model and traps acting between nucleotides (see hairpin_forces.dat for details of the traps)"
+#     echo $TRAPFILE
+#     $OXDNA_BIN $TRAPFILE
+# else
+#     echo "Can't find $OXDNA_BIN, did you compile oxDNA?"
+#     exit
 
-fi
+# fi
 
 # # # ファイルの移動
 # echo "move input and output files to results directory"
