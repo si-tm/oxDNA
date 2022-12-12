1af7956238350ed782ba528a6c64eea0897922aa
Si-tm <57259352+si-tm@users.noreply.github.com>
2022-12-12 22:39:10 +0900

    Update make_samples_ked_2.sh

--- run_samples/make_samples_ked_2.sh
+++ run_samples/make_samples_ked_2.sh
@@ -46,7 +46,7 @@ echo "$INPUTFILE fixed"
 sed -i -e "s|TOP|$TOPFILE|" $INPUTFILE
 sed -i -e "s|.dat|_${now}.dat|" $INPUTFILE
 sed -i -e "s|CONF|$CONFFILE|" $INPUTFILE
-sed -i -e "/steps = 2000000/c steps = 10" $INPUTFILE
+sed -i -e 's|steps = 2000000|steps = 10|' $INPUTFILE
 
 # input_seq_depファイル書き換え
 
@@ -55,8 +55,8 @@ echo "$SEQDEPFILE fixed"
 sed -i -e "s|TOP|$TOPFILE|" $SEQDEPFILE
 sed -i -e "s|.dat|_${now}.dat|" $SEQDEPFILE
 sed -i -e "s|CONF|$CONFFILE|" $SEQDEPFILE
-sed -i -e "/seq_dep_file = ../oxDNA1_sequence_dependent_parameters.txt/c seq_dep_file = oxDNA1_sequence_dependent_parameters.txt" $SEQDEPFILE
-sed -i -e "/steps = 2000000/c steps = 10" $SEQDEPFILE
+sed -i -e 's|seq_dep_file = ../oxDNA1_sequence_dependent_parameters.txt|seq_dep_file = oxDNA1_sequence_dependent_parameters.txt' $SEQDEPFILE
+sed -i -e 's|steps = 2000000|steps = 10|' $SEQDEPFILE
 
 # input_trapファイル書き換え
 
@@ -66,8 +66,8 @@ sed -i -e "s|TOP|$TOPFILE|" $TRAPFILE
 sed -i -e "s|.dat|_${now}.dat|" $TRAPFILE
 sed -i -e "s|CONF|$CONFFILE|" $TRAPFILE
 sed -i -e "s|FORCE|${FORCESFILE}|" $TRAPFILE
-sed -i -e "/external_forces = 1/c external_forces = 0" $TRAPFILE # forceファイルを使わない設定
-sed -i -e "/steps = 2000000/c steps = 10" $TRAPFILE
+sed -i -e 's|external_forces = 1|external_forces = 0|' $TRAPFILE # forceファイルを使わない設定
+sed -i -e 's|steps = 2000000|steps = 10|' $TRAPFILE
 
 # 実行
 OXDNA_BIN=build/bin/oxDNA
