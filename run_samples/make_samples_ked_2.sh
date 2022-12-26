cc132a473e661b076467db609fb86a3025a09839
Si-tm <57259352+si-tm@users.noreply.github.com>
2022-12-26 11:04:18 +0900

    Update make_samples_ked_2.sh
    step change 10000000 to 10

--- run_samples/make_samples_ked_2.sh
+++ run_samples/make_samples_ked_2.sh
@@ -46,7 +46,7 @@ echo "$INPUTFILE fixed"
 sed -i -e "s|TOP|$TOPFILE|" $INPUTFILE
 sed -i -e "s|.dat|_${now}.dat|" $INPUTFILE
 sed -i -e "s|CONF|$CONFFILE|" $INPUTFILE
-sed -i -e 's|STEPS|10000000|' $INPUTFILE
+sed -i -e 's|STEPS|10|' $INPUTFILE
 
 # input_seq_depファイル書き換え
 
@@ -56,7 +56,7 @@ sed -i -e "s|TOP|$TOPFILE|" $SEQDEPFILE
 sed -i -e "s|.dat|_${now}.dat|" $SEQDEPFILE
 sed -i -e "s|CONF|$CONFFILE|" $SEQDEPFILE
 sed -i -e 's|seq_dep_file = ../oxDNA1_sequence_dependent_parameters.txt|seq_dep_file = oxDNA1_sequence_dependent_parameters.txt' $SEQDEPFILE
-sed -i -e 's|STEPS|10000000|' $SEQDEPFILE
+sed -i -e 's|STEPS|10|' $SEQDEPFILE
 
 # input_trapファイル書き換え
 
@@ -67,7 +67,7 @@ sed -i -e "s|.dat|_${now}.dat|" $TRAPFILE
 sed -i -e "s|CONF|$CONFFILE|" $TRAPFILE
 sed -i -e "s|FORCE|${FORCESFILE}|" $TRAPFILE
 sed -i -e 's|external_forces = 1|external_forces = 0|' $TRAPFILE # forceファイルを使わない設定
-sed -i -e 's|STEPS|10000000|' $TRAPFILE
+sed -i -e 's|STEPS|10|' $TRAPFILE
 
 echo "tmp2.sh"
 cat tmp2.sh
