81e1a8635c85b10bb61c52e1ccdf30a4852af32e
Si-tm <57259352+si-tm@users.noreply.github.com>
2023-01-30 22:42:33 +0900

    Update run_oxdna_random_5.sh

--- run_oxdna_random_5.sh
+++ run_oxdna_random_5.sh
@@ -52,8 +52,8 @@ make_input () {
     sed -i -e "s|last_conf|${dir_path}/last_conf_${target}.dat|" $INPUTFILE
 
     sed -i -e "s|CONF|$CONFFILE|" $INPUTFILE
-    # sed -i -e 's|STEPS|200000|' $INPUTFILE
-    sed -i -e 's|STEPS|20|' $INPUTFILE
+    sed -i -e 's|STEPS|200000|' $INPUTFILE
+    # sed -i -e 's|STEPS|20|' $INPUTFILE
 
     # echo "$SEQDEPFILE fix"
 
@@ -67,8 +67,8 @@ make_input () {
 
     sed -i -e "s|CONF|$CONFFILE|" $SEQDEPFILE
     sed -i -e 's|seq_dep_file = ../oxDNA1_sequence_dependent_parameters.txt|seq_dep_file = oxDNA1_sequence_dependent_parameters.txt' $SEQDEPFILE
-    # sed -i -e 's|STEPS|200000|' $SEQDEPFILE
-    sed -i -e 's|STEPS|20|' $SEQDEPFILE
+    sed -i -e 's|STEPS|200000|' $SEQDEPFILE
+    # sed -i -e 's|STEPS|20|' $SEQDEPFILE
 
     # echo "$TRAPFILE fix"
 
@@ -83,8 +83,8 @@ make_input () {
     sed -i -e "s|CONF|$CONFFILE|" $TRAPFILE
     sed -i -e "s|FORCE|${FORCESFILE}|" $TRAPFILE
     sed -i -e 's|external_forces = 1|external_forces = 0|' $TRAPFILE # forceファイルを使わない設定
-    # sed -i -e 's|STEPS|200000|' $TRAPFILE
-    sed -i -e 's|STEPS|20|' $TRAPFILE
+    sed -i -e 's|STEPS|200000|' $TRAPFILE
+    # sed -i -e 's|STEPS|20|' $TRAPFILE
 
 }
 
