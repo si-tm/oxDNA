e1772b96622cdef54a99f42ef7f1e1f1e61ab9d4
Si-tm <57259352+si-tm@users.noreply.github.com>
2023-01-30 22:43:02 +0900

    Update run_oxdna_random_6.sh

--- run_oxdna_random_6.sh
+++ run_oxdna_random_6.sh
@@ -2,7 +2,7 @@
 
 echo $1
 
-for i in `seq 1 1`
+for i in `seq 1 10`
 do
     # ./run_oxdna_random_4.sh "results_soturon/L1/d-0-1/L1_d-0-1_2023-01-27-083608/L1_d-0-1_2023-01-27-083608/"
     # ./run_oxdna_random_4.sh $1
