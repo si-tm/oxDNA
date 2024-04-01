d7bdadf68a3f01350f2ffda7bba1a1a792d09d01
si-tm <kumomaya@gmail.com>
2024-04-01 23:53:55 +0900

    run fromQD L まとめて

---
+++ run_fromQD_L.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# ./run_fromQD_L.sh L2_0
+# L2_0以下のrから始まるreqのリストを取る
+# それぞれのreqに対し、 ./run_fromQD.sh L1_0 r1711781544506-0 25 を実行する
+
+target=$1
+box_size=$2
+req_lst=`ls conf/$1`
+pattern='_[^.]*.'
+
+for req in $req_lst
+do
+    if [[ $req == r* ]]; then
+        echo $req
+        if [[ $req =~ _([^_.]+)\. ]]; then
+            extracted_string="${BASH_REMATCH[1]}"
+            echo "抽出された文字列は: $extracted_string"
+            ./run_fromQD.sh $target $extracted_string $box_size
+        else
+            echo "該当するパターンが見つかりませんでした。"
+        fi
+    fi
+done
