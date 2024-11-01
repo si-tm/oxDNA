4f02ba4d9ea29d010095f6358b09fbd1e10c0cda
si-tm <kumomaya@gmail.com>
2024-11-01 11:51:22 +0900

    modify

--- run_req_iteration_all.sh
+++ run_req_iteration_all.sh
@@ -18,6 +18,7 @@ for dir in "$target_dir"/*; do
     name="${file#req_}"     # 先頭の req_ を削除
     name="${name%.txt}"     # 末尾の .txt を削除
     echo $name
-    ./run_req_iteration.sh conf/req_L1_1/r1730373350439-94911 r1730373350439-94911 25 1 10
+    # ./run_req_iteration.sh conf/req_L1_1/r1730373350439-94911 r1730373350439-94911 25 1 10
     # echo "./run_req_iteration.sh $dir $name $box_size $iteration $step $iteration_step"
+    ./run_req_iteration.sh $dir $name $box_size $iteration $step $iteration_step
 done
