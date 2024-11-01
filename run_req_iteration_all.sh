5f600e42d79d4c950f149ba2d45c099041ca5e8e
si-tm <kumomaya@gmail.com>
2024-11-01 11:58:10 +0900

    modify iteration step

--- run_req_iteration_all.sh
+++ run_req_iteration_all.sh
@@ -8,7 +8,7 @@ target_dir=$1 # "conf/req_L1_1"
 box_size=$2 # L1,L2: 25, L3: 50
 iteration=$3 # 1-10
 step=$4 # L1,L2 1000000, L3 5000000
-step=$5 # L1,L2 100000, L3 500000
+iteration_step=$5 # L1,L2 100000, L3 500000
 
 # ディレクトリ内の全てのファイルに対して処理を実行
 for dir in "$target_dir"/*; do
