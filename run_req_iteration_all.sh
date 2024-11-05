0097d39977dc9c5c8253ac1153d3b5e4ec43a08c
si-tm <kumomaya@gmail.com>
2024-11-05 23:01:24 +0900

    wow

--- run_req_iteration_all.sh
+++ run_req_iteration_all.sh
@@ -22,5 +22,5 @@ for dir in "$target_dir"/*; do
     echo $name
     # ./run_req_iteration.sh conf/req_L1_1/r1730373350439-94911 r1730373350439-94911 25 1 10
     echo "./run_req_iteration.sh $dir $name $box_size $iteration $step $iteration_step"
-    # ./run_req_iteration.sh $dir $name $box_size $iteration $step $iteration_step
+    ./run_req_iteration.sh $dir $name $box_size $iteration $step $iteration_step
 done
